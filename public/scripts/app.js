e569ea6cd1949eec1d9e1efbeb6ea1ccd7e52cc9
ejasquith <emily.asquith@icloud.com>
2022-08-15 01:24:40 +0100

    Render dynamic values in JSX

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,11 +2,56 @@
 
 console.log('App.js running!');
 
-var template = React.createElement(
-  'h1',
-  null,
-  'Indecision App'
-);
-var appRoot = document.getElementById('app');
+var app = {
+    title: 'Indecision',
+    subtitle: 'Put your life in the hands of a computer'
+};
 
+var template = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        app.title
+    ),
+    React.createElement(
+        'p',
+        null,
+        app.subtitle
+    )
+);
+
+var user = {
+    fname: 'Emily',
+    lname: 'Asquith',
+    age: 22,
+    location: 'Kendal, UK'
+};
+
+var userInfoTemplate = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        user.fname,
+        ' ',
+        user.lname
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age: ',
+        user.age
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location: ',
+        user.location
+    )
+);
+
+var appRoot = document.getElementById('app');
 ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,6 +1,31 @@
 console.log('App.js running!');
 
-var template = <h1>Indecision App</h1>;
-var appRoot = document.getElementById('app');
+let app = {
+    title: 'Indecision',
+    subtitle: 'Put your life in the hands of a computer'
+}
 
+let template = (
+    <div>
+        <h1>{app.title}</h1>
+        <p>{app.subtitle}</p>
+    </div>
+);
+
+let user = {
+    fname: 'Emily',
+    lname: 'Asquith',
+    age: 22,
+    location: 'Kendal, UK'
+};
+
+let userInfoTemplate = (
+    <div>
+        <h1>{user.fname} {user.lname}</h1>
+        <p>Age: {user.age}</p>
+        <p>Location: {user.location}</p>
+    </div>
+);
+
+let appRoot = document.getElementById('app');
 ReactDOM.render(template, appRoot);
